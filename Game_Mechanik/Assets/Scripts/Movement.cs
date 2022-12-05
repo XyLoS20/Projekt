e294d81d321ea3bfc0eeff9d46fe2840d095cc86
XyLoS <pawelkowolik777@gmail.com>
2022-12-05 17:43:47 +0100

    Test

--- Game_Mechanik/Assets/Scripts/Movement.cs
+++ Game_Mechanik/Assets/Scripts/Movement.cs
@@ -9,9 +9,9 @@ public class Movement : MonoBehaviour
     private Vector3 moveDirection = Vector3.zero;
     CharacterController characterController;
 
-    public float jumpSpeed = 8.0f;
+    public float jumpSpeed = 6.0f;
     public float gravity = 20.0f;
-    public float speed = 5.0f;
+    public float speed = 15.0f;
 
     // Start is called before the first frame update
     void Start()
